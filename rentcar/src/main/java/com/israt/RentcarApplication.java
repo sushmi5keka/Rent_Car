7d3d7ab39abcc49797196cc593aeff66bd8831fe
sushmi5keka <sushmi5keka@gmail.com>
2018-12-26 13:44:02 +0600

    ok

---
+++ rentcar/src/main/java/com/israt/RentcarApplication.java
@@ -0,0 +1,14 @@
+package com.israt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentcarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RentcarApplication.class, args);
+    }
+
+}
+
